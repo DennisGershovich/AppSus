711ab2a82bca25ce8b0de1167554f528f73b9a8b
Denis_Gershovich <denis.gershovich@gmail.com>
2022-04-27 23:29:50 +0300

    updated emails nav

--- apps/email/cmps/email-folder-list.jsx
+++ apps/email/cmps/email-folder-list.jsx
@@ -1,11 +1,15 @@
+const { NavLink } = ReactRouterDOM
 
 export function EmailFolderList(){
 
-    return  <div className="email-nav-container">
+    return <div className="email-nav-container">
     <ul>
-             <li>Filter</li>
-             <li>Filter</li>
-            <li>Filter</li>
-        </ul>
-    </div>
+        <li className="email-nav-item"><NavLink to="">Compose</NavLink></li>
+        <li className="email-nav-item"><NavLink to="">Inbox</NavLink></li>
+        <li className="email-nav-item"><NavLink to="">Started</NavLink></li>
+        <li className="email-nav-item"><NavLink to="">Sent</NavLink></li>
+        <li className="email-nav-item"><NavLink to="">Drafts</NavLink></li>
+        <li className="email-nav-item"><NavLink to="">Trash</NavLink></li>
+    </ul>
+</div>
 }
