ec527f9bbc141fa63a17d602c86985b568e34549
Uri Baynesay <uri552@gmail.com>
2022-04-27 12:48:32 +0300

    loading of the notes to the dom

--- cmps/notes-cmp/note-list.jsx
+++ cmps/notes-cmp/note-list.jsx
@@ -1,7 +1,26 @@
-export class NotesList extends React.Component{
-    render(){
-        return <section className="notes-list-container">
-            <h1>hello from the list</h1>
-        </section>
-    }
+import { noteService } from "../../services/note.service.js";
+export class NotesList extends React.Component {
+  state = {
+    notes: null,
+  };
+
+  componentDidMount() {
+    this.loadNotes();
+  }
+
+  loadNotes = () => {
+    noteService.query().then((res) => {
+      this.setState({notes:res});
+    });
+  };
+
+  render() {
+      const {notes}=this.state
+      if(!notes) return <React.Fragment></React.Fragment>
+    return (
+      <section className="notes-list-container">
+        <h1>hello from the list</h1>
+      </section>
+    );
+  }
 }
