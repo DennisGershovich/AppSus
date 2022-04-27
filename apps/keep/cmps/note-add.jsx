63e4777001219dc51f740312ff3416f6c671568a
Uri Baynesay <uri552@gmail.com>
2022-04-27 23:13:58 +0300

    start of add feature

--- apps/keep/cmps/note-add.jsx
+++ apps/keep/cmps/note-add.jsx
@@ -1,7 +1,36 @@
-export class AddNote extends React.Component{
-    render(){
-        return <div className="add-note-container">
-            <input type="text" />
-        </div>
-    }
+export class AddNote extends React.Component {
+  state = {
+    value: "",
+    noteType :null
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({ value: target.value });
+  };
+
+  onSubmit=(ev)=>{
+    ev.preventDefault();
+    const {value,noteType}=this.state
+    if(!value||!noteType) return;
+  }
+
+  onSetType=(ev,type)=>{
+      if(!this.state.value) ev.preventDefault();
+      this.setState({noteType:type})
+  }
+
+  render() {
+    let { value } = this.state;
+    return (
+      <div className="add-note-container">
+        <form onSubmit={this.onSubmit}>
+          <input type="text" value={value} onChange={this.handleChange} />
+          <button className="btn-add-text-note" onClick={(ev)=>this.onSetType(ev,'txt')}>txt</button>
+          <button className="btn-add-img-note" onClick={(ev)=>this.onSetType(ev,'img')}>img</button>
+          <button className="btn-add-todo-note" onClick={(ev)=>this.onSetType(ev,'todo')}>todo</button>
+          <button className="btn-add-vid-note" onClick={(ev)=>this.onSetType(ev,'vid')}>vid</button>
+        </form>
+      </div>
+    );
+  }
 }
--- apps/keep/cmps/note-preview.jsx
+++ apps/keep/cmps/note-preview.jsx
@@ -23,7 +23,7 @@ export class NotePreview extends React.Component {
     return (
       <React.Fragment>
         <DynamicCmp type={note.type} note={note}/>
-        <input name="note-color" type="color" onChange={(ev)=>this.onChangeBgcColor(ev.target)}/>
+        <input name="note-color" value={`${note.style?note.style.backgroundColor:''}`} type="color" onChange={(ev)=>this.onChangeBgcColor(ev.target)}/>
       </React.Fragment>
     );
   }
