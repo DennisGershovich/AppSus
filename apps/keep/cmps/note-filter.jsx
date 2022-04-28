e855341ec2835659eccd93a878c54a7dab4399fb
Uri Baynesay <uri552@gmail.com>
2022-04-28 16:32:41 +0300

    start of filter feature

--- apps/keep/cmps/note-filter.jsx
+++ apps/keep/cmps/note-filter.jsx
@@ -1,1 +1,26 @@
-//hello
+export class NotesFilter extends React.Component{
+    state={
+        filterBy:{
+            search:'',
+            type:''
+        }
+    }
+
+    handleChange=({target})=>{
+        this.setState((prevState)=>({filterBy:{...prevState.filterBy,[target.name]:target.value}}))
+    }
+
+    render(){
+        let {search,type}=this.state.filterBy
+        return <section className="filter-container">
+            <input type="text" name="search" value={search} placeholder="Search..." onChange={this.handleChange}/>
+            <select name="" id="" value={type}>
+                <option value="">All</option>
+                <option value="text-note">Text</option>
+                <option value="img-note">Images</option>
+                <option value="todo-note">Todo</option>
+                <option value="vid-note">Videos</option>
+            </select>
+        </section>
+    }
+}
--- pages/app-notes.jsx
+++ pages/app-notes.jsx
@@ -1,11 +1,13 @@
 import { AddNote } from "../apps/keep/cmps/note-add.jsx";
 import { NotesList } from "../apps/keep/cmps/note-list.jsx";
+import { NotesFilter } from "../apps/keep/cmps/note-filter.jsx";
 
 export class Notes extends React.Component{
 
     render(){
         return <main className="notes-app-container flex">
             <AddNote/>
+            <NotesFilter/>
             <NotesList/>
         </main>
     }
