0564e748a33695d81f7e908f2106c05ce9656051
Denis_Gershovich <denis.gershovich@gmail.com>
2022-05-01 19:15:14 +0300

    added expand button title

--- apps/email/cmps/email-read-more.jsx
+++ apps/email/cmps/email-read-more.jsx
@@ -8,7 +8,7 @@ export function ReadMore({email,OnRemoveEmail}){
         <h4>email:{email.to}</h4>
         <p>{email.body}</p>
         <div className="read-more-links-container">
-        <Link  className="take-to-email-details-btn" to={`/email/${email.id}`}> <i className="fas fa-expand icon" ></i> </Link>
+        <Link  className="take-to-email-details-btn" to={`/email/${email.id}`}> <i className="fas fa-expand icon" title='expand'></i> </Link>
         <Link to={`/notes?emailId=${email.id}`}  ><i className="note-pin fas fa-thumbtack icon"  title="Save as note"></i> </Link> 
         <button className="remove-btn" onClick={() => OnRemoveEmail(email.id)}> <i className=" remove-btn fas fa-trash-alt"></i>   </button>
         </div>
